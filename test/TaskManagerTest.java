ff99b31f56712241dfe60b65c9ff5b44e4156a08
Plastinin-Igor <igor-plastinin@yandex.ru>
2025-01-21 18:35:20 +0300

    Добавить тесты для класса InMemoryTaskManager: обновление статусов эпика, время и продолжительность эпика, пересечение интервалов

---
+++ test/TaskManagerTest.java
@@ -0,0 +1,5 @@
+import model.TaskManager;
+
+public abstract class TaskManagerTest<T extends TaskManager> {
+
+}
--- test/util/InMemoryHistoryManagerTest.java
+++ test/util/InMemoryHistoryManagerTest.java
@@ -5,14 +5,17 @@ import org.junit.jupiter.api.Assertions;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.Collection;
 
 class InMemoryHistoryManagerTest {
 
     HistoryManager historyManager = Managers.getDefaultHistory();
 
-    Task taskObj1 = new Task("Задача № 1", "Описание задачи №1", Status.NEW);
-
+    //Простая задача
+    Task taskObj1 = new Task(1, "Задача № 1", "Описание задачи №1", Status.NEW,
+            Duration.ofMinutes(15), LocalDateTime.of(2025, 1, 1, 10, 15));
 
     @Test
     void add() {
@@ -37,5 +40,4 @@ class InMemoryHistoryManagerTest {
         historyList = historyManager.getHistory();
         Assertions.assertTrue(historyList.isEmpty(), "История не удалена");
     }
-
 }
